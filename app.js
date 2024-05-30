9bb19204ba406eac507ab749e79e4ddd47513679
Abhinav Vellalacheruvu <146443683+vellaabh000@users.noreply.github.com>
2024-05-30 16:08:07 -0400

    Javascript practices.
    Day1

---
+++ app.js
@@ -0,0 +1,8 @@
+// New javascript practices for console.log
+/* DAY 1 */
+
+
+console.log(8*5);
+console.log(8+3);
+console.log("Coding is awesome".length)
+console.log("Stay, I need you and stay god".toUpperCase())
